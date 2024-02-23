7981510613bd17114f92d201d54ac86c1b24f0f5
GovindRobo <buttargovind@gmail.com>
2024-02-23 14:21:59 -0800

    Change PigeonId for real robot

--- src/main/java/frc/robot/subsystems/Swerve/GyroIOPigeon2.java
+++ src/main/java/frc/robot/subsystems/Swerve/GyroIOPigeon2.java
@@ -25,10 +25,11 @@ import com.ctre.phoenix6.hardware.Pigeon2;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import frc.robot.Constants;
+import frc.robot.Constants.RobotType;
 
 /** IO implementation for Pigeon2 */
 public class GyroIOPigeon2 implements GyroIO {
-  private final Pigeon2 pigeon = new Pigeon2(15);
+  private final Pigeon2 pigeon = Constants.getRobot() == RobotType.ROBOT_2024C ? new Pigeon2(10) : new Pigeon2(15); 
   private final StatusSignal<Double> yaw = pigeon.getYaw();
   private final Queue<Double> yawPositionQueue;
   private final Queue<Double> yawTimestampQueue;
