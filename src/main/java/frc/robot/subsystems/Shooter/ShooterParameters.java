b2abd77ab3cbe1189fa05ceea925de7b7d25a3ba
Sajiv Fan91 <hyoga.esu@gmail.com>
2024-03-06 10:40:39 -0800

    Shooter Parameters

---
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterParameters.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.Shooter;
+
+public class ShooterParameters {
+    public static double voltage_to_kRPM(double voltage){
+        return 0.508779*voltage-0.148742;
+    }
+    public static  double kRPM_to_voltage(double kRPM){
+        return 1/0.508779*kRPM+0.148742/0.508779;
+    }
+    public static  double voltage_to_mps(double voltage){
+        return 2.18115*voltage-1.59467;
+    }
+    public static  double mps_to_voltage(double mps){
+        return 1/2.18115*mps+1.59467/2.18115;
+    }
+    public static  double mps_to_kRPM(double mps){
+        return voltage_to_kRPM(mps_to_voltage(mps));
+    }
+}
